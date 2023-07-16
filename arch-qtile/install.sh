67092ec2ee06f3376dc9a962e3a6b3ce65b5f12f
João <jlcarruda3@gmail.com>
2023-07-16 10:17:11 -0300

    feature: added functions to install AUR, pip and base packages from txt files

--- arch-qtile/install.sh
+++ arch-qtile/install.sh
@@ -3,6 +3,8 @@ VERSION="1.0.0"
 NODE_VERSION="18"
 VIDEO_DRIVER="xf86-video-vmware"
 BASE_PACKAGES_FILE='base-packages.txt'
+AUR_PACKAGES_FILE='aur-packages.txt'
+PIP_PACKAGES_FILE="pip-packages.txt"
 
 screen_size=$(stty size 2>/dev/null || echo 24 80)
 rows=$(echo $screen_size | awk '{print $1}')
@@ -13,16 +15,25 @@ r=$(( rows / 2 ))
 c=$(( columns / 2 ))
 
 check_install_dependencies() {
-  sudo pacman -S extra/libnewt
+  sudo pacman -S --noconfirm extra/libnewt python-pip "$VIDEO_DRIVER"
 }
 
-base ()
+install_base_packages()
 {
   echo "::::: Installing base packages :::::"
   while read -r package; do
     echo "Installing ${package}..."
     sudo pacman -S --noconfirm "$package"
   done < "$BASE_PACKAGES_FILE"
+
+  echo ":::"
+  echo "Instalation complete!"
+}
+
+baseInstall(){
+  whiptail --msgbox --backtitle "GIGA Installer $VER" --title "Base Packages Install" "The base packages needed for GIGA to work will be installed now \n \n Click OK to Continue" ${r} ${c}
+    
+  whiptail --title "Package Installation" --gauge "Installing ..." 6 50 0 < <(install_base_packages) 2>&1
 }
 
 paru_install(){
@@ -34,6 +45,51 @@ paru_install(){
   rm -rf paru
 }
 
+install_aur_packages()
+{
+  echo "::::: Installing AUR Packages :::::"
+  while read -r package; do
+    echo "Installing AUR package ${package}..."
+    paru -S --noconfirm "$package"
+  done < "$AUR_PACKAGES_FILE"
+
+  echo ":::"
+  echo "Instalation complete!"
+}
+
+aur_install(){
+  whiptail --msgbox --backtitle "GIGA Installer $VER" --title "AUR Packages Install" "The base packages needed for GIGA to work will be installed now \n \n Click OK to Continue" ${r} ${c}
+    
+  whiptail --title "AUR Package Installation" --gauge "Installing ..." 6 50 0 < <(install_aur_packages) 2>&1
+}
+
+install_pip_packages()
+{
+  TOTAL_PACKAGES=$(wc -l < "$PIP_PACKAGES_FILE")
+  count=0
+  
+  while read -r PACKAGE; do
+    ((count++))
+
+    pip install -r "${$PIP_PACKAGES_FILE}" --break-system-packages
+
+    PERCENTAGE=$((count * 100 / TOTAL_PACKAGE))
+
+    echo "::: "
+    echo "Installing $PACKAGE..."
+    echo "$PERCENTAGE"
+    sleep 1
+  done < "$PIP_PACKAGES_FILE"
+
+  echo "::: "
+  echo "Installation complete!"
+}
+
+pip_install() {
+  whiptail --msgbox --backtitle "GIGA Installer $VER" --title "Pip Packages Install" "The base packages needed for GIGA to work will be installed now \n \n Click OK to Continue" ${r} ${c}
+    
+  whiptail --title "Pip Package Installation" --gauge "Installing ..." 6 50 0 < <(install_pip_packages) 2>&1
+}
 
 installMenu(){
   CHOICES=$(whiptail --menu "GIGA Rice Installation" ${r} ${c} 5 \
@@ -46,17 +102,16 @@ installMenu(){
   for CHOICE in $CHOICES; do
     case "$CHOICE" in
     "1")
-      echo "Option 1 was selected"
-      #baseInstall >&2
+      base_install >&2
       ;;
     "2")
-      echo "Option 2 was selected"
+      pip_install >&2
       ;;
     "3")
-      echo "Option 3 was selected"
+      paru_install >&2
+      aur_install >&2
       ;;
     "4")
-      echo "Option 4 was selected"
       ;;
     *)
       echo "Unsupported item $CHOICE!" >&2
@@ -64,7 +119,6 @@ installMenu(){
       ;;
     esac
   done
-
 }
 
 check_install_dependencies
